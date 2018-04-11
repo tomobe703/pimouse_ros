7b7a826afa55def03505eefa8b385f615f8e0c87
H.T <tomobe703@gmail.com>
2018-04-11 16:03:51 +0000

    修正

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -3,10 +3,10 @@
 #required packages
 #sudo pip install --upgrade pip
 
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+#pip install catkin_pkg
+#pip install empy
+#pip install pyyaml
+#pip install rospkg
 
 #ros install
 cd ..
@@ -17,7 +17,7 @@ bash ./step1.bash
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
-cd catkin_ws/src
+cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
